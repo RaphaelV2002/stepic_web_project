06fbc12aa313088714f222a8131104c95adcf1a9
RaphaelV2002 <raphael200280@gmail.com>
2022-07-04 12:42:17 +0400

    g

--- etc/hello.py
+++ etc/hello.py
@@ -1,6 +1,2 @@
-def app(env, start_response):
-    data = env['QUERY_STRING'].split('&')
-    data = '\n'.join(data)
-    data = bytes(data, encoding='utf-8')
-    start_response('200 OK', [('Content-Type', 'text/plain')] )
-    return [ data ]
+bind="0.0.0.0:8080"
+pythonpath="/home/box/web/"
