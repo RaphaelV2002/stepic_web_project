e86bc5ce48e71feb51ea9dbb3e2bcefb383c3568
RaphaelV2002 <raphael200280@gmail.com>
2022-07-04 11:57:27 +0400

    h

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
-gunicorn -c hello.py hello:app
+gunicorn --bind='0.0.0.0:8080' hello:app
